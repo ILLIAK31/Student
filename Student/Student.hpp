cbfe8d887f078442f30afa6ae2a3af6cb7ac733f
Illia <karma@MYPC>
2023-05-12 21:51:09 +0200

    100%

--- Student/Student.hpp
+++ Student/Student.hpp
@@ -4,7 +4,7 @@ class Student
 {
 private:
 	string surname, name , album_num , grupe;
-	int year;
+	int year{};
 public:
 	Student() = default;
 	void Set_Value(const string element, const int index);
